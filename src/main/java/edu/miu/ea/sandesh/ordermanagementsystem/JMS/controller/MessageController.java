0a16a258a63289a52bea6b34de851ee5c95bb243
Sandesh Shahi <SandeshShahi@Sandeshs-MacBook-Pro.local>
2024-12-20 21:05:38 -0600

    final commit fix jms

--- src/main/java/edu/miu/ea/sandesh/ordermanagementsystem/JMS/controller/MessageController.java
+++ src/main/java/edu/miu/ea/sandesh/ordermanagementsystem/JMS/controller/MessageController.java
@@ -2,12 +2,15 @@ package edu.miu.ea.sandesh.ordermanagementsystem.JMS.controller;
 
 import edu.miu.ea.sandesh.ordermanagementsystem.JMS.entity.OrderInfo;
 import edu.miu.ea.sandesh.ordermanagementsystem.JMS.producer.Producer;
+import edu.miu.ea.sandesh.ordermanagementsystem.Order.entity.Order;
+import edu.miu.ea.sandesh.ordermanagementsystem.Order.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
+import java.util.Optional;
 
 @RestController()
 @RequestMapping("/message")
@@ -15,11 +18,21 @@ public class MessageController {
     @Autowired
     private Producer producer;
 
-    @GetMapping(path = "/orders")
-    public OrderInfo getOrders() {
-        OrderInfo orderInfo = new OrderInfo(1L, LocalDateTime.now().minusDays(3), 25.30);
-        producer.sendOrder(orderInfo);
-        return orderInfo;
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @GetMapping(path = "/orders/{id}")
+    public OrderInfo getOrders(@PathVariable Long id) {
+        Optional<Order> order = orderRepository.findById(id);
+        if (order.isPresent()) {
+            OrderInfo orderInfo = new OrderInfo();
+            orderInfo.setId(order.get().getId());
+            orderInfo.setOrderDate(order.get().getOrderDate());
+            orderInfo.setTotalPrice(order.get().getTotalPrice());
+            producer.sendOrder(orderInfo);
+            return orderInfo;
+        }
+        return null;
     }
 
 
